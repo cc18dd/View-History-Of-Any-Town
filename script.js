abc38761af47a5b8427ede0ac5ba88a634f205af
cc18dd <coolcraftnet18@gmail.com>
2023-09-29 08:02:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,16 @@ function lookupTown() {
                 <td><a href="https://www.google.com/search?q=site:planecrashmap.com%20${encodeURIComponent(townName)}">Link</a></td>
                 <td></td>
             </tr>
+            <tr>
+                <td>Wikipedia</td>
+                <td><a href="https://en.wikipedia.org/wiki/${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>US Census</td>
+                <td><a href="https://data.census.gov/all?q=${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
+            </tr>
         `;
     } else {
         resultsTableBody.innerHTML = `

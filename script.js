029bc3e66406bb0c182f86245c058dbb00289d05
cc18dd <coolcraftnet18@gmail.com>
2023-09-28 21:51:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 function lookupTown() {
-    // Get the town name from the input field
     var townName = document.getElementById("townName").value.trim();
+    var resultsTableBody = document.getElementById("resultsTableBody");
 
-    // Check if the town name is not empty
     if (townName !== "") {
-        // Create links to the services based on the town name
-        var topoViewLink = '<li><a href="https://ngmdb.usgs.gov/topoview/?q=' + encodeURIComponent(townName) + '">TopoView</a></li>';
-        var vintageAerialLink = '<li><a href="https://vintageaerial.com/photos/search?q=' + encodeURIComponent(townName) + '">VintageAerial</a></li>';
-        var cityDataLink = '<li><a href="https://www.city-data.com/search/?&siteurl=www.city-data.com/#&gsc.q=' + encodeURIComponent(townName) + '">City-Data</a></li>';
-        var extremeWeatherWatchLink = '<li><a href="https://www.extremeweatherwatch.com/cities/' + encodeURIComponent(townName) + '">ExtremeWeatherWatch</a></li>';
-        var planeCrashMapLink = '<li><a href="https://www.google.com/search?q=site:planecrashmap.com%20' + encodeURIComponent(townName) + '">PlaneCrashMap</a></li>';
-
-        // Display the links in the results list
-        var resultsList = document.getElementById("results");
-        resultsList.innerHTML = topoViewLink + vintageAerialLink + cityDataLink + extremeWeatherWatchLink + planeCrashMapLink;
+        resultsTableBody.innerHTML = `
+            <tr>
+                <td>TopoView</td>
+                <td><a href="https://ngmdb.usgs.gov/topoview/?q=${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>VintageAerial</td>
+                <td><a href="https://vintageaerial.com/photos/search?q=${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>City-Data</td>
+                <td><a href="https://www.city-data.com/search/?&siteurl=www.city-data.com/#&gsc.q=${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>ExtremeWeatherWatch</td>
+                <td><a href="https://www.extremeweatherwatch.com/cities/${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>PlaneCrashMap</td>
+                <td><a href="https://www.google.com/search?q=site:planecrashmap.com%20${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
+            </tr>
+        `;
     } else {
-        // If the input is empty, show an error message
-        var resultsList = document.getElementById("results");
-        resultsList.innerHTML = "<li>Please enter a town name.</li>";
+        resultsTableBody.innerHTML = `
+            <tr>
+                <td colspan="3">Please enter a town name.</td>
+            </tr>
+        `;
     }
 }
-    

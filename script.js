1bfb3a2498ccf0c6dad8074a209e5f950759ceb8
cc18dd <coolcraftnet18@gmail.com>
2023-09-29 08:37:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 function lookupTown() {
     var townName = document.getElementById("townName").value.trim();
     var resultsTableBody = document.getElementById("resultsTableBody");
-    var metaDescriptionTableBody = document.getElementById("metaDescriptionTableBody");
 
     if (townName !== "") {
         resultsTableBody.innerHTML = `
             <tr>
                 <td>TopoView</td>
-                <td><a href="https://ngmdb.usgs.gov/topoview/?q=${encodeURIComponent(townName)}" target="_blank">Link</a></td>
-                <td id="topoViewMetaDescription"></td>
+                <td><a href="https://ngmdb.usgs.gov/topoview/?q=${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
             </tr>
-            <!-- Add similar rows for other links -->
+            <tr>
+                <td>VintageAerial</td>
+                <td><a href="https://vintageaerial.com/photos/search?q=${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>City-Data</td>
+                <td><a href="https://www.city-data.com/search/?&siteurl=www.city-data.com/#&gsc.q=${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>ExtremeWeatherWatch</td>
+                <td><a href="https://www.extremeweatherwatch.com/cities/${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>PlaneCrashMap</td>
+                <td><a href="https://www.google.com/search?q=site:planecrashmap.com%20${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Wikipedia</td>
+                <td><a href="https://en.wikipedia.org/wiki/${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>US Census</td>
+                <td><a href="https://data.census.gov/all?q=${encodeURIComponent(townName)}">Link</a></td>
+                <td></td>
+            </tr>
         `;
-
-        // Fetch meta descriptions for each link
-        fetchMetaDescription("https://ngmdb.usgs.gov/topoview/?q=" + encodeURIComponent(townName), "topoViewMetaDescription");
-        // Add similar fetchMetaDescription calls for other links
     } else {
         resultsTableBody.innerHTML = `
             <tr>
@@ -23,24 +47,4 @@ function lookupTown() {
             </tr>
         `;
     }
-}
-
-// Function to fetch meta description and update the table cell
-function fetchMetaDescription(url, cellId) {
-    fetch(url)
-        .then(response => response.text())
-        .then(html => {
-            // Parse the HTML to extract the meta description
-            var parser = new DOMParser();
-            var doc = parser.parseFromString(html, "text/html");
-            var metaDescription = doc.querySelector('meta[name="description"]');
-            var descriptionContent = metaDescription ? metaDescription.getAttribute("content") : "No description available";
-
-            // Update the table cell with the meta description
-            document.getElementById(cellId).textContent = descriptionContent;
-        })
-        .catch(error => {
-            console.error("Error fetching meta description:", error);
-            // Handle errors if necessary
-        });
 }

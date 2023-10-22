b3ca7387d7fd7f6fde4f1749764b319789d7a1ff
cc18dd <coolcraftnet18@gmail.com>
2023-10-22 19:23:43 -0400

    Update script.js
    Changed how hyperlinks look

--- script.js
+++ script.js
@@ -6,48 +6,48 @@ function lookupTown() {
         resultsTableBody.innerHTML = `
             <tr>
                 <td>VintageAerial</td>
-                <td><a href="https://vintageaerial.com/photos/search?q=${encodeURIComponent(townName)}">Link</a></td>
+                <td><a href="https://vintageaerial.com/photos/search?q=${encodeURIComponent(townName)}">Old Aerial Photos for ${townName}</a></td>
                 <td></td>
             </tr>
             <tr>
                 <td>City-Data</td>
-                <td><a href="https://www.city-data.com/search/?&siteurl=www.city-data.com/#&gsc.q=${encodeURIComponent(townName)}">Link</a></td>
+                <td><a href="https://www.city-data.com/search/?&siteurl=www.city-data.com/#&gsc.q=${encodeURIComponent(townName)}">Info about ${townName}</a></td>
                 <td></td>
             </tr>
             <tr>
                 <td>ExtremeWeatherWatch</td>
-                <td><a href="https://www.extremeweatherwatch.com/cities/${encodeURIComponent(townName)}">Link</a></td>
+                <td><a href="https://www.extremeweatherwatch.com/cities/${encodeURIComponent(townName)}">${townName} Weather Data</a></td>
                 <td></td>
             </tr>
             <tr>
                 <td>PlaneCrashMap</td>
-                <td><a href="https://www.google.com/search?q=site:planecrashmap.com%20${encodeURIComponent(townName)}">Link</a></td>
+                <td><a href="https://www.google.com/search?q=site:planecrashmap.com%20${encodeURIComponent(townName)}">${townName} Plane Crashes</a></td>
                 <td></td>
             </tr>
             <tr>
                 <td>Wikipedia</td>
-                <td><a href="https://en.wikipedia.org/wiki/${encodeURIComponent(townName)}">Link</a></td>
+                <td><a href="https://en.wikipedia.org/wiki/${encodeURIComponent(townName)}">${townName} - Wikipedia</a></td>
                 <td></td>
             </tr>
             <tr>
                 <td>US Census</td>
-                <td><a href="https://data.census.gov/all?q=${encodeURIComponent(townName)}">Link</a></td>
+                <td><a href="https://data.census.gov/all?q=${encodeURIComponent(townName)}">US Census Search</a></td>
                 <td></td>
             </tr>
             <tr>
                 <td>Google Maps</td>
-                <td><a href="https://www.google.com/maps?q=${encodeURIComponent(townName)}">Link</a></td>
-                <td><a href="https://google.com/maps/search/${encodeURIComponent(townName)}">Link</a></td>
+                <td><a href="https://www.google.com/maps?q=${encodeURIComponent(townName)}">Launch Google Maps</a></td>
+                <td><a href="https://google.com/maps/search/${encodeURIComponent(townName)}">Launch Google Maps</a></td>
                 
             </tr>
             <tr>
                 <td>Facebook</td>
-                <td><a href="https://www.facebook.com/search/?q=${encodeURIComponent(townName)}%20History">Link</a></td>
-                <td><a href="https://www.facebook.com/search/?q=If you grew up in ${encodeURIComponent(townName)}%20then you know">Link</a></td>
+                <td><a href="https://www.facebook.com/search/?q=${encodeURIComponent(townName)}%20History">${townName} History</a></td>
+                <td><a href="https://www.facebook.com/search/?q=If you grew up in ${encodeURIComponent(townName)}%20then you know">If you grew up in ${townName} then you know</a></td>
             </tr>
             <tr>
                 <td>TopoView</td>
-                <td><a href="https://ngmdb.usgs.gov/topoview/">Link</a></td>
+                <td><a href="https://ngmdb.usgs.gov/topoview/">Launch Topoview</a></td>
                 <td></td>
             </tr>
         `;
